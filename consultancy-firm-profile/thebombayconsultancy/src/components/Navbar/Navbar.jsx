471fbe259fd4af89a7947af1e6182417e8271088
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-09-04 22:42:04 +0530

    Add assests tot he service section & adjust the Navbar section at mobile view

--- consultancy-firm-profile/thebombayconsultancy/src/components/Navbar/Navbar.jsx
+++ consultancy-firm-profile/thebombayconsultancy/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-import { getImageUrl } from '../../utils';
-//for click_navigation and smooth react scrolling
-import {Link} from 'react-scroll';
+import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
-    const[showNavbar, setShowNavbar] = useState(true)
-    const[lastScrollY, setLastScrollY] = useState(0)
-  return (
-   <section>
-    <nav className={styles.navbar}>
-        <a className={styles.title} href="/"><img  src={getImageUrl("nav/tbh_navbarimage.png")} alt="" /></a>
-       
-        <button className={styles.menuBtn} 
-                        onClick={() => setMenuOpen(!menuOpen)}>
-                        <img 
-                            src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} 
-                            alt="menu-button"
-                        />
-                    </button>
-    </nav>
-    {menuOpen && (
-                <div className={styles.menuContainer}>
-                    <ul className={styles.menuItems}>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#service">Service</a></li>
-                        <li><a href="#articles">Articles</a></li>
-                        <li><a href="#team">Team</a></li>
-                        <li><a href="#career">Career</a></li>
-                        <li><a href="#contact-us">Contact Us</a></li>
-                    </ul>
-                </div>
-            )}
-        
-    
-   </section> 
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+  return (
+    <section>
+      <nav className={styles.navbar}>
+        <a className={styles.title} href="/">
+          <img src={getImageUrl("nav/tbh_navbarimage.png")} alt="Logo" />
+        </a>
+
+        <div className={styles.menu}>
+          <img
+            className={styles.menuBtn}
+            src={
+              menuOpen
+                ? getImageUrl("nav/closeIcon.png")
+                : getImageUrl("nav/menuIcon.png")
+            }
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)}
+          />
+
+          {/* Menu items container */}
+          <ul
+            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}
+          >
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#service">Service</a>
+            </li>
+            <li>
+              <a href="#articles">Articles</a>
+            </li>
+            <li>
+              <a href="#team">Team</a>
+            </li>
+            <li>
+              <a href="#career">Career</a>
+            </li>
+            <li>
+              <a href="#contact-us">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </section>
+  );
+};
+
+export default Navbar;
