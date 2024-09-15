387bb26c795269b7c14c63522a7ba5f9536c56bd
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-09-15 18:22:11 +0530

    Add handleScrollLock and ScrollY functionality

--- consultancy-firm-profile/thebombayconsultancy/src/components/Navbar/Navbar.jsx
+++ consultancy-firm-profile/thebombayconsultancy/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from 'react-scroll';
 import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  //handle scroll: freeze and unfreeze
+  useEffect( () => {
+    
+      if(window.innerWidth <= 900){
+        if(menuOpen){
+          document.body.style.overflow = 'hidden';
+        }
+        else{document.body.style.overflow = 'auto';}
+      }
+      else{
+        document.body.style.overflow = 'auto';
+      }
+    }
+  )
+
+  
+  const [lastScrollY, setLastScrollY] = useState(0); // lastScrollY: recent verticle scroll is set to 0(px) bcz by default.
+  const [showNavbar, setShowNavbar] = useState(true); // by default showNavbar is true. Bcz it has to be visible initially
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.innerWidth > 100){ // we want this functionality for both desktop and mobile
+        if(window.scrollY > lastScrollY){
+          // scrolling down = hide navbar bcz increase in pixel of verticle scrolling
+          setShowNavbar(false);
+        }
+        // scrolling up - show navbar
+        else{setShowNavbar(true)}
+        setLastScrollY(window.scrollY);
+      }
+      
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    // clean up function
+    return() =>{
+      window.removeEventListener('scroll', handleScroll)
+    }
+    
+  }, [lastScrollY])
+   
+    
+  
   
   return (
     <section>
-      <nav className={styles.navbar}>
+      <nav className={`${styles.navbar} ${showNavbar ? '' : styles.navbarHidden}`}>
         <a className={styles.title} href="/">
           <img src={getImageUrl("nav/company_logo.png")} alt="Logo" />
         </a>
