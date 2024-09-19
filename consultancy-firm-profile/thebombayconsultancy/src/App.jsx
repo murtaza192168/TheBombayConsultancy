eec8a5c827ccaadec646b2d927c9f4fd305b56f7
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-09-19 20:59:40 +0530

    Add preventDrag functionality on mousedown event

--- consultancy-firm-profile/thebombayconsultancy/src/App.jsx
+++ consultancy-firm-profile/thebombayconsultancy/src/App.jsx
@@ -1,13 +1,24 @@
-
+import { useEffect } from 'react'; 
 import styles from './App.module.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Service from './components/Service/Service'
-import Career from './components/Career/Career';
+// import Career from './components/Career/Career';
 import About from './components/About/About';
 import Footer from './components/Footer/Footer';
 function App() {
- 
+  useEffect(() => {
+    const preventDrag = (event) => {
+      event.preventDefault();
+    };
+  
+    document.addEventListener('mousedown', preventDrag);
+  
+    return () => {
+      document.removeEventListener('mousedown', preventDrag);
+    };
+  }, []);
+  
 
   return (
     <div className={styles.App}>
